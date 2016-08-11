7441d84ca786e899f7a7e11f8087b09754f936e0
Alaor <alaor.resende@gmail.com>
2016-08-11 12:57:56 -0300

    múltiplos pontos no mapa.

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
 import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 
+//jQuery
+declare var jQuery:any;
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+  info: string;
+}
+
 @Component({
   moduleId: module.id,
   selector: 'app-home',
@@ -14,7 +26,22 @@ export class HomeComponent {
   lat: number = -20.626959;
   lng: number = -45.154981;
 
-  constructor() { }
+  markers: marker[] = [
+    {
+      lat: -20.478638,
+      lng: -45.120549,
+      label: 'B',
+      draggable: true,
+      info: 'Point B'
+    },
+    {
+      lat: -20.626959,
+      lng: -45.154981,
+      label: 'Alaor',
+      draggable: false,
+      info: 'Point A'
+    }
+  ]
 
 
 }
