afa6553a0108a0cb4539828796c62b14fc851c6a
Alaor <alaor.resende@gmail.com>
2016-08-15 18:05:20 -0300

    mapa buscando do banco.

--- project/src/app/home/home.component.ts
+++ project/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
+
+import { EmployersService } from '../employers/employers.service';
 
 //jQuery
 declare var jQuery:any;
@@ -18,30 +20,41 @@ interface marker {
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
+  providers: [EmployersService],
   directives: [GOOGLE_MAPS_DIRECTIVES]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   title: string = 'Meu primeiro mapa';
   lat: number = -20.626959;
   lng: number = -45.154981;
 
-  markers: marker[] = [
-    {
-      lat: -20.478638,
-      lng: -45.120549,
-      label: 'B',
-      draggable: true,
-      info: 'Point B'
-    },
-    {
-      lat: -20.626959,
-      lng: -45.154981,
-      label: 'Alaor',
-      draggable: false,
-      info: 'Point A'
-    }
-  ]
+  markers: marker[] = [];
 
+  constructor(private employersService: EmployersService) {}
+
+  ngOnInit(){
+    this.getEmployers();
+  }
+
+  getEmployers(){
+    this.employersService.getData()
+      .subscribe(
+        data => {
+          for (let key in data) {
+
+            this.markers.push({
+              lat: Number(data[key].lat),
+              lng: Number(data[key].lng),
+              info: data[key].info,
+              label: data[key].label,
+              draggable: false
+            })
+
+          }
+        },
+        error => console.log(error)
+      );
+  }
 
 }
